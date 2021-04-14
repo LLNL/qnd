e5124e7fd64c16cebf872c8eaf56c8992ae27b60
Jay D. Salmonson <salmonson1@llnl.gov>
2021-04-13 23:09:43 -0700

    added example files

---
+++ examples/example_write_h5.py
@@ -0,0 +1,36 @@
+'''
+example_write_h5.py demos how to write single values and multiple records 
+into an hdf5 file
+'''
+import numpy as np
+from qnd.h5f import openh5
+
+fname = "foo.h5"
+
+with openh5(fname, "w") as f:
+
+    # write a single value:
+    f.x = 3
+
+    # turn on recording and write two records:
+    f.recording(1)
+    f.tm = 0
+    f.y = 0.1
+    f.z = np.arange(3)
+
+    f.tm = 1
+    f.y = 0.2
+    f.z = np.arange(3)+1
+    
+# reopen file to append data:
+with openh5(fname, "a") as f:
+    
+    # append a single new variable to the file
+    f.a = 10
+
+    # write one new record:
+    f.recording(1)
+    f.tm = 2
+    f.y = 0.3
+    f.z = np.arange(3)+2
+
---
+++ examples/example_write_pdb.py
@@ -0,0 +1,33 @@
+'''
+example_write_pdb.py demos how to write single values and multiple records 
+into a family of pdb files
+'''
+import numpy as np
+from qnd.pdbf import openpdb
+
+with openpdb("foo000.pdb", "w") as f:
+
+    # write a single value:
+    f.x = 3
+
+    # turn on recording and write two records:
+    f.recording(1)
+    f.tm = 0
+    f.y = 0.1
+    f.z = np.arange(3)
+
+    f.tm = 1
+    f.y = 0.2
+    f.z = np.arange(3)+1
+
+# append a single new variable to the file:
+with openpdb("foo000.pdb", "a") as f:
+    f.a = 10
+
+# open a new file in the family and write one new record:
+with openpdb("foo001.pdb", "w") as f:
+    f.recording(1)
+    f.tm = 2
+    f.y = 0.3
+    f.z = np.arange(3)+2
+
