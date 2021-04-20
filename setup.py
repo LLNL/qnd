891dbd44bb4cd87f44c5b5ea734ee360cefaff66
David Strozzi <strozzi2@llnl.gov>
2021-04-20 10:12:39 -0700

    setup.py: added middle initial, to see if readthedocs webhook triggers.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="qnd",
     version="0.9.0",
-    author="David Strozzi",
+    author="David J. Strozzi",
     author_email="strozzi2@llnl.gov",
     description="Quick n Dirty Python package for working with binary files",
     url="https://github.com/LLNL/qnd",
